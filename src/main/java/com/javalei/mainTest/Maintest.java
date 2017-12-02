c83c72c48d2c46668c194a5ad39d8a5595026e7a
wuyanzu <45446717@qq.com>
2017-12-02 22:25:21 +0800

    note

--- src/main/java/com/javalei/mainTest/Maintest.java
+++ src/main/java/com/javalei/mainTest/Maintest.java
@@ -5,6 +5,7 @@ import sun.applet.Main;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Callable;
 
 /***
  *
@@ -14,13 +15,14 @@ public class Maintest {
         System.out.println("我是main的代码库");
     }
                 public    String msg="this is mess";
-                private static class test implements Runnable{
+                private static class test implements Callable{
                  static{
                         System.out.println("我是test的代码库");
                     }
+                   
                     @Override
-                    public void run() {
-                        System.out.println(new Maintest().msg);
+                    public Object call() throws Exception {
+                        System.out.println("--");
                     }
                 }
 
